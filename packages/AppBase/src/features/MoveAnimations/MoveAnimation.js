f49ec9131ef031bd74e38e79930ea09fab386ddb
Cat Binh Le <>
2022-06-30 18:51:19 +0700

    Added sequence animation

--- packages/AppBase/src/features/MoveAnimations/MoveAnimation.js
+++ packages/AppBase/src/features/MoveAnimations/MoveAnimation.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text, Animated, TouchableOpacity} from 'react-native'
+import { View, Text, Animated, TouchableOpacity, Easing} from 'react-native'
 import styles from './styles';
 
 export const MoveAnimationScreen = () => {
-    const positionY = new Animated.Value(-200)
+    const position = new Animated.ValueXY({x: -150, y: 300})
     const FadeInView = () => (
         <Animated.Image                
         style={{
             width: 100, 
             height: 100, 
             resizeMode: 'contain',
-            transform: [{ translateX: 0}, {translateY: positionY}],
+            transform: [{ translateX: position.x}, {translateY: position.y}],
         }}
         source={require('../../../assets/bird.png')}
       />
@@ -26,12 +26,41 @@ export const MoveAnimationScreen = () => {
                     style={{height: 44, margin: 20, backgroundColor: 'powderblue'}}
                     onPress={() => {
                         Animated.timing(
-                            positionY,
+                            position,
                             {
-                              toValue: 0,
+                              toValue: {x: 0, y: -200},
                               duration: 5000,
+                              easing: Easing.bounce
                             }
                           ).start();
+                        // Animated.decay(position, {
+                        //     // coast to a stop
+                        //     velocity: {x: 20, y: -20 }, // velocity from gesture release
+                        //     deceleration: 0.1,
+                        //     }).start();
+                        // Animated.sequence([
+                        //     Animated.timing(
+                        //         position,
+                        //         {
+                        //           toValue: {x: -150, y: 0},
+                        //           duration: 5000,
+                        //         }
+                        //     ),
+                        //     Animated.timing(
+                        //         position,
+                        //         {
+                        //           toValue: {x: 0, y: 0},
+                        //           duration: 1000,
+                        //         }
+                        //     ),
+                        //     // Animated.decay(position, {
+                        //     //     // coast to a stop
+                        //     //     velocity: {x: 20, y: 0 }, // velocity from gesture release
+                        //     //     deceleration: 0.1,
+                        //     // })
+                            
+                        //   ]).start();
+                        
                     }}
                 >
                     <Text>Click</Text>
